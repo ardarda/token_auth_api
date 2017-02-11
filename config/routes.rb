438a935ca176b272dbbe056dc46201c2cbddb18d
Arda Cicek <ardacck@gmail.com>
2017-02-11 19:18:24 +0300

    registrations withdrawel

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   resources :works
   resources :employers
   resources :books
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: "registrations" }
+  mount_devise_token_auth_for 'User', at: 'auth' #, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
